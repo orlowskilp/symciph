95760e3e9bb5e80f045b10d058b367997d6aa64a
orlowskilp <orlowskilp@gmail.com>
2023-11-11 22:33:41 +0800

    chore: Implemented DES encryption strategy

--- src/strategy.rs
+++ src/strategy.rs
@@ -1,3 +1,7 @@
+mod des;
+
+use des::DesEncryptionStrategy;
+
 use super::{DataChunk, EncryptionSchemes};
 
 pub trait EncryptionStrategy {
@@ -6,8 +10,11 @@ pub trait EncryptionStrategy {
 }
 
 impl EncryptionSchemes {
-    pub fn _strategy(&self, _key_bytes: &[DataChunk]) -> Box<dyn EncryptionStrategy> {
-        todo!("Implement factory method")
+    pub fn _strategy(&self, key_bytes: &[DataChunk]) -> Box<dyn EncryptionStrategy> {
+        match self {
+            Self::Des => Box::new(DesEncryptionStrategy::_new(key_bytes)),
+            _ => todo!("Implement remaining encryption strategies"),
+        }
     }
 
     pub const fn key_size(&self) -> usize {
@@ -19,4 +26,13 @@ impl EncryptionSchemes {
             Self::Aes256 => 4,
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    pub const KEY_64_BITS: [DataChunk; 1] = [[0x6F, 0x2B, 0x91, 0xE7, 0x4F, 0xD8, 0xA9, 0x5C]; 1];
+    pub const PLAINTEXT_64_BITS: [DataChunk; 1] =
+        [[0x4A, 0x7F, 0x22, 0xC3, 0x8D, 0x5E, 0xA1, 0x3B]; 1];
 }
---
+++ src/strategy/des.rs
@@ -0,0 +1,79 @@
+use des::DesCipher;
+
+use crate::DataChunk;
+
+use super::EncryptionStrategy;
+
+const _DES_KEY_CHUNKS: usize = 1;
+
+pub(super) struct DesEncryptionStrategy {
+    scheme: DesCipher,
+}
+
+impl DesEncryptionStrategy {
+    pub fn _new(key_bytes: &[DataChunk]) -> Self {
+        assert!(
+            key_bytes.len() == _DES_KEY_CHUNKS,
+            "DES: Key not 64 bits long"
+        );
+
+        let key = key_bytes[0];
+        let scheme = DesCipher::new(&key);
+
+        Self { scheme }
+    }
+}
+
+impl EncryptionStrategy for DesEncryptionStrategy {
+    fn encrypt(&self, plaintext: &[DataChunk]) -> Vec<DataChunk> {
+        let mut ciphertext_blocks: Vec<DataChunk> = Vec::new();
+
+        for plaintext_block in plaintext {
+            ciphertext_blocks.push(self.scheme.encrypt(plaintext_block));
+        }
+
+        ciphertext_blocks
+    }
+
+    fn decrypt(&self, ciphertext: &[DataChunk]) -> Vec<DataChunk> {
+        let mut plaintext_blocks: Vec<DataChunk> = Vec::new();
+
+        for ciphertext_block in ciphertext {
+            plaintext_blocks.push(self.scheme.decrypt(ciphertext_block));
+        }
+
+        plaintext_blocks
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::strategy::tests::*;
+
+    use super::*;
+
+    const DES_CIPHERTEXT: [DataChunk; _DES_KEY_CHUNKS] =
+        [[0x4E, 0x7F, 0x27, 0xC3, 0x9C, 0x0F, 0xF0, 0x2B]; _DES_KEY_CHUNKS];
+
+    #[test]
+    fn encrypt() {
+        let strategy = DesEncryptionStrategy::_new(&KEY_64_BITS);
+        let ciphertext = strategy.encrypt(&PLAINTEXT_64_BITS);
+
+        let left = ciphertext[0];
+        let right = DES_CIPHERTEXT[0];
+
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn decrypt() {
+        let strategy = DesEncryptionStrategy::_new(&KEY_64_BITS);
+        let plaintext = strategy.decrypt(&DES_CIPHERTEXT);
+
+        let left = plaintext[0];
+        let right = PLAINTEXT_64_BITS[0];
+
+        assert_eq!(left, right);
+    }
+}
